ed09cf534d843ab85a94414334a3610d223645ca
Mark Yaraskavitch <mark@yaraskavitch.com>
2025-03-22 20:57:09 -0400

    test: integrate MSW for API mocking in setupTests.js; configure server lifecycle management

--- setupTests.js
+++ setupTests.js
@@ -1,5 +1,12 @@
 import { expect, vi, test } from "vitest";
+import { setupServer } from "msw/node";
 
 globalThis.expect = expect;
 globalThis.vi = vi;
 globalThis.test = test;
+
+const server = setupServer();
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
