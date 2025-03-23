7c6551a0e716e78f33922a14b0a11822517fc0cc
Mark Yaraskavitch <mark@yaraskavitch.com>
2025-03-22 22:25:49 -0400

    feat: restructure application to include Apollo and React Query providers in index file; simplify routing in App component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,22 @@
-import { ApolloProvider } from "@apollo/client";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, NavLink } from "react-router";
-import { client } from "../clients/apollo";
+import { Routes, Route, NavLink } from "react-router";
 import { Home } from "../pages/Home/Home";
 import { MissionLauncher } from "../pages/Mission/MissionLauncher";
 import { ProductListPage } from "../pages/ProductList/ProductListPage";
-import { queryClient } from "../clients/react-query";
 
 export const App = () => {
   return (
-    <ApolloProvider client={client}>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/mission">Last Mission</NavLink>
-            <NavLink to="/products">Products</NavLink>
-          </nav>
+    <>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/mission">Last Mission</NavLink>
+        <NavLink to="/products">Products</NavLink>
+      </nav>
 
-          <Routes>
-            <Route path="" element={<Home />} />
-            <Route path="mission" element={<MissionLauncher />} />
-            <Route path="products" element={<ProductListPage />} />
-          </Routes>
-        </BrowserRouter>
-      </QueryClientProvider>
-    </ApolloProvider>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="mission" element={<MissionLauncher />} />
+        <Route path="products" element={<ProductListPage />} />
+      </Routes>
+    </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./app/page";
+import { BrowserRouter } from "react-router";
+import { client } from "./clients/apollo";
+import { queryClient } from "./clients/react-query";
+import { ApolloProvider } from "@apollo/client";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
