2f4a8aac15ec36ef3cd4b27fefcfec87242911bc
Mark Yaraskavitch <mark@yaraskavitch.com>
2025-03-25 21:56:06 -0400

    feat: refactor ConfirmOrderButton to use useNavigate for navigation and update Shop state on click

--- src/pages/Shop/components/OrderModal/ConfirmOrderButton.tsx
+++ src/pages/Shop/components/OrderModal/ConfirmOrderButton.tsx
@@ -1,10 +1,20 @@
-import { Link } from "react-router";
+import { Link, useLocation, useNavigate } from "react-router";
 import { paths } from "../../../../app/AppRouter.constants";
+import { useShop } from "../../ShopState";
 
 export const ConfirmOrderButton = () => {
-  return (
-    <Link to={paths.order.confirmation.replace(":orderId", "123")}>
-      <button>Yes, Buy Now!</button>
-    </Link>
-  );
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    useShop.setState({
+      orderModal: {
+        isOpen: false,
+        productId: null,
+      },
+    });
+
+    navigate(paths.order.confirmation.replace(":orderId", "123"));
+  };
+
+  return <button onClick={handleClick}>Yes, Buy Now!</button>;
 };
