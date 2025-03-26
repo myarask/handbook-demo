56043ee9b7bf32a88334a4571622e29f75ff8374
Mark Yaraskavitch <mark@yaraskavitch.com>
2025-03-25 21:56:18 -0400

    feat: refactor ConfirmOrderButton to remove unused imports and streamline navigation

--- src/pages/Shop/components/OrderModal/ConfirmOrderButton.tsx
+++ src/pages/Shop/components/OrderModal/ConfirmOrderButton.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { paths } from "../../../../app/AppRouter.constants";
 import { useShop } from "../../ShopState";
 
